ac886e64c6d6d54471b7dd7c6831f3a410118752
Erik Grimmbråten <erik.grimmbraten@polestar.com>
2020-10-02 17:05:57 +0200

    :sparkles: Add get target type helper

--- helpers/checkers.sh
+++ helpers/checkers.sh
@@ -1,3 +1,4 @@
+# $1: any (value to be checked)
 isNaN() {
  if ! [[ "$1" =~ ^[0-9]+$ ]]; then
   echo true;
@@ -6,6 +7,7 @@ isNaN() {
  fi
 }
 
+# $1: string (value to be checked)
 isFile() {
  if [[ $1 = *.* ]]; then
   echo true;
@@ -14,6 +16,17 @@ isFile() {
  fi
 }
 
+# $1: string (value to be checked)
+getTargetType() {
+ if $(isFile $1); then
+  echo "file";
+ else
+  echo "folder";
+ fi
+}
+
+# $1: string (value to be checked)
+# $2: string (path to text file)
 existsInFile() {
  if grep -Fxq "$1" $2; then
   echo true;
@@ -22,6 +35,8 @@ existsInFile() {
  fi
 }
 
+# $1: string (value to search in)
+# $2: string (value to search for)
 contains() {
  if [[ $1 == *"$2"* ]]; then
   echo true;
