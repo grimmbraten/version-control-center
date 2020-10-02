22dc65e02071704fcc29920bd57cbd6864e5da81
Erik Grimmbråten <erik.grimmbraten@polestar.com>
2020-10-02 15:05:16 +0200

    :sparkles: Add additional console icons for prompts

--- helpers/icons.sh
+++ helpers/icons.sh
@@ -1,17 +1,30 @@
-local alarm="\U0001F6A8"
-local boom="\U0001F4A5"
-local car="\U0001F699"
-local construction="\U0001F6A7"
-local disappointed="\U0001F614"
-local evergreen="\U0001F332";
-local holup="\U0001F928"
-local hourglass="\U00023F3"
-local keylock="\U0001F510"
-local package="\U0001F4E6"
-local plant="\U0001F33F"
-local seedling="\U0001F331"
-local ship="\U0001F6A2"
-local tada="\U0001F389"
-local thinking="\U0001F914"
-local tree="\U0001F333"
-local truck="\U0001F69A"
+# general icons
+local alarmIcon="\U0001F6A8";
+local Icon="\U0001F4A5";
+local constructionIcon="\U0001F6A7";
+local descriptionIcon="\U0001F4D6";
+local labelIcon="\U0001F3F7";
+local leafsIcon="\U0001F343";
+local lockIcon="\U0001F512";
+local microscopeIcon="\U0001F52C";
+local packageIcon="\U0001F4E6";
+local Icon="\U0001F389";
+local telescopeIcon="\U0001F52D";
+local thinkingIcon="\U0001F914";
+local skepticIcon="\U000U1F928";
+local surprisedIcon="\U000U1F62E";
+
+# push/pull size icons
+local rocketIcon="\U0001F680";
+local shipIcon="\U0001F6A2";
+local airplaneIcon="\U0001F6EB";
+local truckIcon="\U0001F69A";
+local carIcon="\U0001F699";
+local scooterIcon="\U0001F6F5";
+local bicycleIcon="\U0001F6B2";
+
+# branch size icons
+local evergreenIcon="\U0001F332";
+local tree=Icon"\U0001F333";
+local plantIcon="\U0001F33F";
+local seedlingIcon="\U0001F331";
