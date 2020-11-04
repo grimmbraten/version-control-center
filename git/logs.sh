a5e398123def23302b172221a010de9d2b9b640f
Erik Grimmbråten <erik.grimmbraten@polestar.com>
2020-11-04 21:33:57 +0100

    update logs function to be verbose

--- git/logs.sh
+++ git/logs.sh
@@ -1,16 +1,7 @@
 logs() {
- spacer;
-
  if [ ! -z $1 ]; then
   invalid "gl";
  else
-  runLogsRequest;
+  git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset';
  fi
-
- spacer;
 }
-
-runLogsRequest() {
- git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset';
-}
-
